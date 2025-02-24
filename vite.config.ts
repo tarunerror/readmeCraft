4999c13fa7a949bfeffafb4ab99bab62ddbee076
Tarun Gautam <113667563+tarunerror@users.noreply.github.com>
2025-02-24 17:46:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/readmecraft/', // Set the base URL for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
